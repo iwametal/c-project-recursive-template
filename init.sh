62a18573edca1de314614f5e3c6ff3cd7feb1e81
iwametal <od.leonardo01@gmail.com>
2025-07-17 04:08:10 +0900

    feat: Improving init.sh file - Adding shell menu for inputs - Option to instatiate new compiler and language standards

--- init.sh
+++ init.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/bash
 
-if [ -n "$1" ] ; then
-	sed -i 's/project_name/'$1'/g' project.conf
+# COLORS
+LILAC="\033[01;36m"
+BLUE="\033[01;34m"
+RESET_COLOR="\e[0m"
 
-	sed -i 's/~\/path\/to/'$(pwd | sed 's,/*[^/]\+/*$,,' | sed 's/\//\\\//g')'/g' project.conf
+project_name=
+default_compiler="gcc"
+default_std="gnu99"
 
-else
-	echo "Please, provide the name of your Project as an argument"
-fi
+while [ -z "$project_name" ]; do
+	echo -en "$LILAC""[Enter your project name]: ""$RESET_COLOR"
+	read project_name
+done
+
+echo "PROJECT NAME: $project_name"
+echo "Adding project name"
+sed -i 's/project_name/'$projec_name'/g' project.conf
+echo "Adding path for the project"
+sed -i 's/~\/path\/to/'$(pwd | sed 's,/*[^/]\+/*$,,' | sed 's/\//\\\//g')'/g' project.conf
+
+echo -en "$LILAC""[Enter your compiler name or Enter to keep default] ""$BLUE""[Default: gcc]: ""$RESET_COLOR"
+read compiler
+
+[ -n "$compiler" ] && [ $compiler != $default_compiler ] && echo "Adding compiler {$compiler}" && sed -i 's/export CC := gcc/export CC := '$compiler'/g' Makefile
+
+echo -en "$LILAC""[Enter the language standards for your compiler or Enter to keep default] ""$BLUE""[Default: gnu99]: ""$RESET_COLOR"
+read std
+
+[ -n "$std" ] && [ $std != $default_std ] && echo "Adding language standards for the compiler {$std}" && sed -i 's/STD := -std=gnu99/STD := '$std'/g' Makefile
+
+echo -e "$LILAC""[Project Base Configured]""$RESET_COLOR"
+echo "Do not forget to take a look in ""$BLUE""Makefile ""$RESET_COLOR""for additional ""$BLUE""Flags""$RESET_COLOR"" and ""$BLUE""Configurations""$RESET_COLOR"
